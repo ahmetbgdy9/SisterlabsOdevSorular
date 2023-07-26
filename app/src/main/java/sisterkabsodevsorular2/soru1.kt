ebe7f7167459d613ac74e41a02a8c78d368ab99d
ahmet <fenerhab@gmail.com>
2023-07-26 20:27:57 +0300

    .

--- app/src/main/java/sisterkabsodevsorular2/soru1.kt
+++ app/src/main/java/sisterkabsodevsorular2/soru1.kt
@@ -1,5 +1,10 @@
 package sisterkabsodevsorular2
 
+// Bir "Dikdörtgen" adlı sınıf oluşturun ve uzunluk ve genişlik gibi özellikleri içeren özellikleri
+// tanımlayın. Ardından, bu sınıftan bir dikdörtgen nesnesi oluşturun ve
+// dikdörtgenin alanını ve çevresini hesaplayan bir fonksiyon yazın.
+
+
 class Dikdortgen(val uzunluk: Int, val genislik: Int) {
 
     fun alan(): Int {
@@ -14,7 +19,6 @@ class Dikdortgen(val uzunluk: Int, val genislik: Int) {
 fun main() {
 
     val dikdortgen = Dikdortgen(9, 6)
-
 
     val alan = dikdortgen.alan()
     val cevre = dikdortgen.cevre()
